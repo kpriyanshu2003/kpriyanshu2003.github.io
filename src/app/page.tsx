2edd273cbae4767d7866eb3f8e50249f20be5d8d
Kumar Priyanshu <az.priyanshu.rockstar@gmail.com>
2024-06-16 15:13:39 +0530

    Responsive + Social Media Icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,54 @@ import Button from "@/ui/Button";
 import Link from "next/link";
 import React from "react";
 import url from "@/constants/url.json";
+import social from "@/constants/social.json";
+import { v4 as uuid } from "uuid";
+import { redirect } from "next/navigation";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="bg-black text-white min-h-screen grid place-items-center">
       <div className="w-screen">
-        {url.map((i, index) => (
-          <div className="flex justify-center m-4" key={index}>
-            <Button className="w-1/2 p-4 rounded-xl bg-gradient-to-r  from-[#005c97] to-[#363795] hover:from-white hover:to-white hover:text-black text-white">
-              <Link href={i.url}>{i.name}</Link>
-            </Button>
+        <div className="grid place-items-center">
+          <Image
+            src="https://avatars.githubusercontent.com/u/109672617"
+            height={150}
+            width={150}
+            alt="profile_pic"
+            className="rounded-full md:scale-100 scale-90"
+          />
+          <div className="my-5 text-center">
+            <div className="text-lg md:text-2xl font-semibold">
+              Kumar Priyanshu
+            </div>
+            <div className="text-sm md:text-lg mt-2">
+              Full Stack Web Developer
+            </div>
           </div>
-        ))}
+        </div>
+        <div className="my-10">
+          <div className="text-center">Social Media</div>
+          {social.map((i) => (
+            <div key={uuid()}>
+              <div className="flex justify-center m-4" key={uuid()}>
+                <Button className="w-full md:w-1/2 p-4 rounded-xl bg-gradient-to-r  from-[#005c97] to-[#363795] hover:from-white hover:to-white hover:text-black text-white">
+                  <Link href={i.url}>{i.name}</Link>
+                </Button>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="my-10">
+          <div className="text-center">Projects</div>
+          {url.map((i) => (
+            <div className="flex justify-center m-4" key={uuid()}>
+              <Button className="w-full md:w-1/2 p-4 rounded-xl bg-gradient-to-r  from-[#005c97] to-[#363795] hover:from-white hover:to-white hover:text-black text-white">
+                <Link href={i.url}>{i.name}</Link>
+              </Button>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
