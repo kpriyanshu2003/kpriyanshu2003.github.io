5e9b3222d4eae7305c88e183b58108df31694298
Kumar Priyanshu <az.priyanshu.rockstar@gmail.com>
2024-06-16 15:52:04 +0530

    Update in JSON

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,16 +6,17 @@ import social from "@/constants/social.json";
 import { v4 as uuid } from "uuid";
 import { redirect } from "next/navigation";
 import Image from "next/image";
+import classNames from "classnames";
 
 export default function Home() {
   return (
     <div className="bg-black text-white min-h-screen grid place-items-center">
-      <div className="w-screen">
+      <div className="w-full">
         <div className="grid place-items-center">
           <Image
             src="https://avatars.githubusercontent.com/u/109672617"
-            height={150}
-            width={150}
+            height={180}
+            width={180}
             alt="profile_pic"
             className="rounded-full md:scale-100 scale-90"
           />
@@ -23,13 +24,13 @@ export default function Home() {
             <div className="text-lg md:text-2xl font-semibold">
               Kumar Priyanshu
             </div>
-            <div className="text-sm md:text-lg mt-2">
-              Full Stack Web Developer
+            <div className="text-sm md:text-lg mt-2 font-light">
+              Full Stack Web Developer | MERN Stack Developer
             </div>
           </div>
         </div>
         <div className="my-10">
-          <div className="text-center">Social Media</div>
+          <div className="text-center text-xl">Socials</div>
           {social.map((i) => (
             <div key={uuid()}>
               <div className="flex justify-center m-4" key={uuid()}>
@@ -41,7 +42,7 @@ export default function Home() {
           ))}
         </div>
         <div className="my-10">
-          <div className="text-center">Projects</div>
+          <div className="text-center text-xl">Projects</div>
           {url.map((i) => (
             <div className="flex justify-center m-4" key={uuid()}>
               <Button className="w-full md:w-1/2 p-4 rounded-xl bg-gradient-to-r  from-[#005c97] to-[#363795] hover:from-white hover:to-white hover:text-black text-white">
